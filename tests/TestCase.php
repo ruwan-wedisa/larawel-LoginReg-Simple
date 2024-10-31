545846627b53a2b50c979ad72d7e67d2e93763fe
Yadeesha Karunathilake <127331008+Yadeeshie@users.noreply.github.com>
2024-10-31 12:20:23 +0530

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,12 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
+/**
+ * Base test case class for all tests in the application.
+ *
+ * This class extends the `Illuminate\Foundation\Testing\TestCase` class
+ * and provides a common setup for testing.
+ */
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
